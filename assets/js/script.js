26d2d7f15dc10a4b62a2df514e4a5ed7c4353fc8
Emmanuelle Baron <emmanuellebaron1@gmail.com>
2021-06-19 16:08:34 +0200

    add modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Page chargée");
+
   document.getElementById("btn-connexion").addEventListener("click", () => {
     document.getElementById("open-modal").classList.add("modal-window");
+  });
+
+  document.getElementById("form-connexion").addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    const data = {
+      firstname: document.getElementById("firstname"),
+      lastname: document.getElementById("lastname"),
+      email: document.getElementById("email"),
+      message: document.getElementById("message"),
+    };
+
+    const response = await axios.post("?", data);
+    console.log(response);
   });
 });
