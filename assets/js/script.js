bbf99a7fbee028ad578770965ed5788509f25f0d
Emmanuelle Baron <emmanuellebaron1@gmail.com>
2021-06-20 14:30:17 +0200

    modal close

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,22 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("open-modal").classList.add("modal-window");
   });
 
+  // document.querySelector("closed-form-connexion").addEventListener("click", () => {
+  //   document.getElementById("open-modal").classList.remove("modal-window");
+  // });
+
   document.getElementById("form-connexion").addEventListener("click", async (event) => {
     event.preventDefault();
     console.log("Btn cliqué");
 
     const data = {
-      firstname: document.getElementById("firstname"),
-      lastname: document.getElementById("lastname"),
-      email: document.getElementById("email"),
-      message: document.getElementById("message"),
+      firstname: document.getElementById("firstname").value,
+      lastname: document.getElementById("lastname").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
     };
 
-    const response = await axios.post("?", data);
-    console.log(response);
+    const response = await axios.post("http://localhost:3000/form", data);
+    console.log(data);
   });
 });
